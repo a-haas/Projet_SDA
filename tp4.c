92ad678035a18fd46e7029b7bd1a61d0513d8e0d
a-haas <antoine.haas@etu.unistra.fr>
2014-11-17 17:31:32 +0100

    "Binary heap"

--- tp4.c
+++ tp4.c
@@ -1,12 +1,64 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
+
 typedef struct tas_binaire{
 	int racine;
 	struct tas_binaire* ag;
 	struct tas_binaire* ad;
-	static int compteur = 0;
 }Noeud,*Tas;
+
+typedef struct tete{
+	int compteur;
+	Tas t;	
+}*Tete;
+
+typedef struct pile{
+	int val;
+	struct pile* suiv;
+}*Pile;
+
+Pile creerPile()
+{
+	Pile p = (Pile)malloc(sizeof(struct pile));
+	p->suiv = NULL;
+	p->val = 0;
+	return p;
+}
+
+Pile adjonctionTete(Pile p, int val)
+{
+	Pile p1 = creerPile();
+	p1->suiv = p;
+	p1->val = val;
+	return p1;
+}
+
+Pile retirerTete(Pile p){
+	Pile p1 = creerPile();
+	if(p == NULL){
+		return NULL;
+	}
+	else{
+		p1 = p->suiv;
+		free(p);
+		return p1;
+	}
+}
+
+int tete(Pile p)
+{
+	if(p==NULL)
+	{
+		printf("Pile vide.\n");
+		return -1;
+	}
+	else
+	{
+		return p->val;
+	}
+}
 
 Noeud creerNoeud(int racine)
 {
@@ -15,3 +67,29 @@ Noeud creerNoeud(int racine)
 	return n;
 }
 
+Tas parcours(Tete t){
+	int cpt = t->compteur;
+	Tas tas = t->t;
+	Pile p = creerPile();
+	int val;
+	while(cpt != 1){
+		cpt = cpt/2;
+		adjonctionTete(p, cpt);
+	}
+	while(p->suiv != NULL){
+		val = tete(p);
+		p = retirerTete(p);
+		if(p->val%2 == 0){
+			tas = tas->ag;
+		}
+		else
+			tas = tas->ad;
+	}
+	return tas;
+}
+
+int main(){
+	Tete t = (Tete)malloc(sizeof(struct tete));
+	t->compteur = 0;	
+	return 0;
+}
